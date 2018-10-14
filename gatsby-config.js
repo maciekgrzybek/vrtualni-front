26af801430f2ed69aa286f821c6b369ab1671a3c
Maciek Grzybek <maciekgrzybek1@gmail.com>
2018-10-14 21:26:43 +0100

    Add plugin to config with test options

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,13 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        url: 'siemano',
+        test: 'costam'
+      }
+    },
     'gatsby-plugin-offline',
   ],
 }
