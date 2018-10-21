c5176e67c31abed909746869dae9307b0bab82c9
Maciek Grzybek <maciek@first10.co.uk>
2018-10-21 22:38:23 +0100

    Update gatsby-node.js
    Create pages from data

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,37 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path');
+
+exports.createPages = ({ graphql, actions }) => {
+
+  const {createPage} = actions;
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allPage {
+          edges {
+            node {
+              id
+              pageData {
+                slug
+              }
+            }
+          }
+        }
+      }`
+    ).then((pages) => {
+        pages.data.allPage.edges.forEach(({node}) => {
+          createPage({
+            path: node.pageData.slug,
+            component: path.resolve('./src/templates/Page.js'),
+            context: {
+              slug: node.pageData.slug
+            }
+          })
+        });
+      resolve();
+    })
+  })
+
+};
